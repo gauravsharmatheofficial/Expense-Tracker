f78ebc109c61ef29b67a480fbe3dfa490d701880
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2025-01-09 20:30:55 +0530

    setup shadcn components

---
+++ src/config/firebaseInit.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+// FireStore Instance
+const db = getFirestore(app);
+
+// Firebase instance
+const auth = getAuth(app);
+
+// instance of the Google provider object
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, app, db, googleProvider };
